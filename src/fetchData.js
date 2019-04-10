db32aa60141772c2ee3f145f74cf8601a5188f47
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-04-10 18:37:46 +0530

    Created New Branch

---
+++ src/fetchData.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+"use strict"
+class RequestService {
+  async getRequest(url) {
+    const newsData = await await fetch(url)
+      .then(data => data.json())
+      .catch(err => newsSource.articlesProvider(err));
+    newsSource.articlesProvider(newsData);
+    import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+      var element = document.createElement('div');
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    }).catch(error => 'An error occurred while loading the component');
+    // console.log(
+    //   _.join(newsData)
+    // );
+  }
+}
+class newsSourceProvider extends RequestService {
+  articlesProvider(data) {
+    const elementId = document.getElementById("newsDetails");
+    let returnHtml = "",
+      uniqueVal = [];
+    if (data.status === "error") {
+      returnHtml = `<div id="error">${data.message}</div>`;
+    } else {
+      data.articles.map(
+        (
+          { author, title, description, publishedAt, url, urlToImage },
+          index
+        ) => {
+          uniqueVal = index === 0 ? `<h1>${author}</h1>` : "";
+          returnHtml += `${uniqueVal}<div class="newsTitle">${title}</div><div class="newsDescription">${description}</div><div class="publishDate">${publishedAt}</div><div class="imageContainer"><a href="${url}" target="_blank"><img src=${urlToImage} /></a></div>`;
+        }
+      );
+    }
+    elementId.innerHTML = returnHtml;
+  }
+}
+
+const requestCall = new RequestService();
+const newsSource = new newsSourceProvider();
+export default requestCall;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import requestCall from "./fetchData.js";
+import './styles/main.scss';
+
+
+"use strict"
+const apiKey = "720c0314e8b2423eb7e1ffca5a1eeeb1";
+let newsCategory = "bbc-news";
+let url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
+
+const sourceSelectBox = document.getElementById("newsSourceBtn");
+sourceSelectBox.addEventListener("click", () => {
+  url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
+  requestCall.getRequest(url);
+});
