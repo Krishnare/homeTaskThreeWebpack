049116cb3f4236d9a8a5b44267cacafd801c0814
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-04-19 07:09:31 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import urlConstructor from "./config.js";
 import "./styles/main.scss";
 class ApiFetcher {
   async fetch(url) {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      return data;
+    } catch (error) {
+      return error.message;
+    } finally {
+    }
   }
 }
 class NewssourceComponent extends ApiFetcher {
